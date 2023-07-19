fe360334e0216e6b866b72bd173702bda736a55f
dat-khoa <khoa.nguyendat@gmail.com>
2023-07-20 00:49:38 +1000

    scrapped custom modal, use react-modal instead

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
 import NavBar from './components/navbar/NavBar';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
